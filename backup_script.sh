80c265e55dfdfcaec499d5c1ea5a0521e7d17063
Alexandru Ologu <alexandru.ologu@binovate.com>
2017-04-28 10:14:47 +0300

    fixed an error where i was geting 500 from fake s3 server

--- backup_script.sh
+++ backup_script.sh
@@ -3,11 +3,11 @@
 #requires following the steps for seting up docker containers and populating db and having awscli instaled
 
 # dump mongodb in volume
-sudo docker exec -it mongodb mongodump -h localhost --port 27017 --db $1 -o shared/mongo_bu;
+sudo docker exec -it mongodb mongodump --db $1 -h localhost --port 27017 -o shared/mongo_bu;
 # random credentials
 export AWS_ACCESS_KEY_ID=1234
 export AWS_SECRET_ACCESS_KEY=1234
 # make new bucket
-aws --endpoint-url='http://localhost:4569' s3 mb s3://fakes3 --region eu-central-1;
+aws --endpoint-url='http://localhost:4567' s3 mb s3://fakes3 --region eu-central-1;
 # copy database dump in bucket
-aws --endpoint-url='http://localhost:4569' s3 cp ~/docker-mongo/mongo_bu s3://fakes3/mongo_bu --recursive;
+aws --endpoint-url='http://localhost:4567' s3 cp ~/docker-mongo/mongo_bu s3://fakes3/mongo_bu --recursive;
